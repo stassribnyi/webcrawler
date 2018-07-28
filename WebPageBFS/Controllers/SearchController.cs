25b5ef52334268220f79e12bb5ea25a5f478e8f8
Stas Sribnyi <stas.sribnyi@gmail.com>
2018-07-28 16:31:20 +0300

    Init

---
+++ WebPageBFS/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPageBFS.Controllers
+{
+    [Route("api/[controller]")]
+    public class SearchController : Controller
+    {
+        // GET api/search/start/https%3A%2F%2Fexample.com%2Fpath%2Fto%2Froot
+        [HttpGet("start/{rootUrl}")]
+        public string Start(string rootUrl)
+        {
+            return "SessionID";
+        }
+
+        // POST api/search/pause/SessionID
+        [HttpPost("pause/{sessionId}")]
+        public void Pause(string sessionId)
+        {
+        }
+
+        // DELETE api/search/stop/SessionID
+        [HttpDelete("stop/{sessionId}")]
+        public void Stop(string sessionId)
+        {
+        }
+    }
+}
